bdb6e05d794356d404fa72f1aef78f72d8b185bd
jingood2 <jingood2@gmail.com>
2022-01-10 05:34:15 +0900

    update product

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,11 +1,14 @@
 import '@aws-cdk/assert/jest';
 import { App } from '@aws-cdk/core';
-import { MyStack } from '../src/lib/my-stack';
+import { SCProductStack, SCProductType } from '../src/lib/sc-portfolio-stack';
 
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new MyStack(app, 'test');
+  const stack = new SCProductStack(app, 'test', {
+    portfolioname: 'NewPortfolio',
+    codeType: SCProductType.CDK,
+  });
 
   expect(stack).not.toHaveResource('AWS::S3::Bucket');
   expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
