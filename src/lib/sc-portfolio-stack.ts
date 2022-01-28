4ea3e452fd5ff458c8c74717031f86debfa2cb6e
jingood2 <jingood2@gmail.com>
2022-01-29 01:35:37 +0900

    Add filter

--- src/lib/sc-portfolio-stack.ts
+++ src/lib/sc-portfolio-stack.ts
@@ -72,11 +72,12 @@ export class SCProductStack extends cdk.Stack {
 
     let product;
 
-    fs.readdirSync(dir).forEach((file) => {
+    fs.readdirSync(dir).filter(file => file.endsWith('json')).forEach((file) => {
 
-      if (!(file.endsWith('json') || file.endsWith('yaml') || file.endsWith('yml' ))) {
-        return;
-      }
+      /* if (!(file.endsWith('json') || file.endsWith('yaml') || file.endsWith('yml' ))) {
+        continue;
+      } */
+
       // builds full path of file
       const fPath = path.resolve(dir, file);
       console.log('fPath:', fPath);
