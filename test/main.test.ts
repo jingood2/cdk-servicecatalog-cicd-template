25ecaf7d2c1b42198e4bf096daf9f1e6a25e3a27
jingood2 <jingood2@gmail.com>
2022-02-08 14:08:11 +0900

    Clean up

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,11 +1,11 @@
 import '@aws-cdk/assert/jest';
 import { App } from '@aws-cdk/core';
-import { SCProductStack } from '../src/lib/sc-portfolio-stack';
+import { SCPortfolioStack } from '../src/lib/sc-portfolio-stack';
 
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new SCProductStack(app, 'test', { scope: 's3' });
+  const stack = new SCPortfolioStack(app, 'test', {});
 
   expect(stack).not.toHaveResource('AWS::S3::Bucket');
   expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
